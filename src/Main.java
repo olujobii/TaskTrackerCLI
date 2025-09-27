2d8e02fec724eadf3cde4c1cce6132a7c5ec7f8e
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-09-27 12:16:45 +0100

    Added args command simple functionality. Next is the file handling

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
+        if(args.length == 0){
+            System.out.println("No arguments passed, program shutting down");
+            return;
+        }
+
+        switch(args[0]){
+            case "add":
+                System.out.println("Task added successfully");
+                break;
+            case "delete":
+                System.out.println("Deleted successfully");
+                break;
+            case "update":
+                System.out.println("Updated successfully");
+                break;
+            case "mark-in-progress":
+                System.out.println("Mark In progress");
+                break;
+            case "mark-done":
+                System.out.println("Mark done");
+                break;
+            case "list":
+                System.out.println("List");
+                break;
+            default:
+                System.out.println("Not a valid command");
+                break;
+        }
     }
 }
---
+++ src/Task.java
@@ -0,0 +1,8 @@
+public class Task {
+    private int id;
+    private int description;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+
+}
