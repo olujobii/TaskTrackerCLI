a4ece0d27f41f6cd56000844e84a321eee799515
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-11-21 21:48:41 +0100

    Task tacker revised version

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-import app.ConsoleUI;
-
 public class Main {
-    public static void main(String[] args) {
-        if(args.length == 0) {
-            System.out.println("No command found");
-            return;
-        }
-
-        ConsoleUI consoleUI = new ConsoleUI(args);
-
-        consoleUI.startApplication();
-        consoleUI.saveTaskToJsonFile();
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
     }
 }
